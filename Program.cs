1e2dea67c9ea7618e1a1144859e8bbd61c54ede4
Yuliya <you@example.com>
2023-09-06 16:18:46 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -3,18 +3,56 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int lastNumt =1;
+// int lastNumt =1;
 
-string PrintNumber(int start, int end)
+// string PrintNumber(int start, int end)
+// {
+//   if(start==end)
+//   {
+//     return start.ToString();
+//   }
+//   return (start + " "+PrintNumber(start-1, end));
+// }
+
+// Console.WriteLine(PrintNumber(num,lastNumt));
+
+// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+System.Console.WriteLine("Введите число M");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите число N");
+int num2 = Convert.ToInt32(System.Console.ReadLine());
+if (num < num2)
 {
-  if(start==end)
-  {
-    return start.ToString();
-  }
-  return (start + " "+PrintNumber(start-1, end));
+    Console.WriteLine(PrintNum(num,num2));
+}
+else
+{
+    System.Console.WriteLine(" Введите число N больше M");
 }
 
-Console.WriteLine(PrintNumber(num,lastNumt));
+string PrintNum(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return(start + " " + PrintNum(start+1, end));
+}
+int sum = SumNaturalNumbers(num, num2);
+int SumNaturalNumbers(int num, int num2)
+    {
+        if (num > num2)
+            return 0;
+        else
+            return num + SumNaturalNumbers(num + 1, num2);
+    }
+Console.WriteLine("Сумма чисел "+sum);
